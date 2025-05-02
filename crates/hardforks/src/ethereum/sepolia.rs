441482ee7de7fd7d85a17fb107e6a7a55770563a
Emilia Hane <elsaemiliaevahane@gmail.com>
2025-05-02 10:35:00 +0200

    Make ethereum fs coherent with optimism fs

---
+++ crates/hardforks/src/ethereum/sepolia.rs
@@ -0,0 +1,16 @@
+//! Sepolia hardfork starting points
+
+/// Paris sepolia hard fork activation block
+pub const SEPOLIA_PARIS_BLOCK: u64 = 1_450_409;
+/// Shanghai sepolia hard fork activation block
+pub const SEPOLIA_SHANGHAI_BLOCK: u64 = 2_990_908;
+/// Cancun sepolia hard fork activation block
+pub const SEPOLIA_CANCUN_BLOCK: u64 = 5_187_023;
+/// Prague sepolia hard fork activation block
+pub const SEPOLIA_SHANGHAI_TIMESTAMP: u64 = 1_677_557_088;
+/// Cancun sepolia hard fork activation timestamp
+pub const SEPOLIA_CANCUN_TIMESTAMP: u64 = 1_706_655_072;
+/// Paris sepolia hard fork activation timestamp
+pub const SEPOLIA_PARIS_TIMESTAMP: u64 = 1_633_267_481;
+/// Prague sepolia hard fork activation timestamp
+pub const SEPOLIA_PRAGUE_TIMESTAMP: u64 = 1_741_159_776;
